856d0b1f159f9c3ad8e73f1a6a67a8c6a89c0fde
Gabriele Marazzi <gabriele.marazzi@gmail.com>
2015-08-10 11:07:10 +0200

    add get token code

---
+++ get_token.php
@@ -0,0 +1,36 @@
+<?php
+/* 
+ * LEAM srl
+ * API v2 - Example code
+ * 
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+ * Author: Gabriele Marazzi - gabriele.marazzi@gmail.com
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+ * 
+ * Get TOken
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+ * use this method recover your access token
+ * the token is used from other API method
+ * and it is erequired.
+ * 
+ */
+
+
+$URI_base = "http://127.0.0.1/modules/api/v2/";
+$RESOURCE = "getToken/";
+
+$url = $URI_base . $RESOURCE ;
+
+$user="xxx"; 
+$password="xxx"; 
+
+$data=array('user'=>$user,'password'=>$password);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS,$data);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response  = curl_exec($ch);
+curl_close($ch);
+
+echo $response;
